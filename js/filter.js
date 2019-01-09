144710a8f2d6b05d8dfe9da9e6b7e847d65d4555
Julien Menten <julien.menten@student.ehb.Be>
2019-01-09 13:46:51 +0100

    Commit 12: Julien
    Implementeren filter functie, aanpassen van bepaalde classenamen(kinderere, jongeren en volwassenen)

--- js/filter.js
+++ js/filter.js
@@ -1,1 +1,37 @@
-// JavaScript Document
+$("#filterBevestiging").click(function(){
+			var begeleiding = $("#specFilter").val();
+			var regio = $("#regioFilter").val();
+			var leeftijd =$("#leeftijdFilter").val();
+			
+			var filter = $(["class*='."+ begeleiding +"."+ leeftijd +"."+ regio +"'"]);
+			
+			$("div.organisatie").css("display", "inline-flex");				
+			$("div.organisatie:not("+"." + begeleiding +"."+ leeftijd +"."+ regio +")").css( "display", "none" );
+	
+	console.log(begeleiding);
+	console.log(leeftijd);
+	console.log(regio);
+});
+
+/*
+Lijst van mogelijke waarden: 
+- begeleiding: 
+	- all
+	- visueel
+	- auditief
+	- neuromotorisch
+	
+- leeftijd:
+	- all
+	- kinderen
+	- jongeren
+	- volwassenen
+	
+- regio
+	- all
+	- brussel
+	- limburg
+	- o-vlaanderen
+	- v-brabant
+	- w-vlaanderen
+*/
