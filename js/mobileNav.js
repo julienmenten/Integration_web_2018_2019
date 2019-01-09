4b907c09c16145db1b9104562ccd0a035a172af8
Julien Menten <julien.menten@student.ehb.Be>
2019-01-09 10:15:26 +0100

    Commit 10
    Navigatie onzichtbaar bij resize na openen en sluiten mobile menu error OPGELOST

--- js/mobileNav.js
+++ js/mobileNav.js
@@ -1,9 +1,24 @@
 var nav = $("#mobileNav");
 
+
 $("#mobileMenuIcon").click(function(){
 	$(nav).slideToggle();
 	$(".mobileIconLijn1").toggleClass('knopAnimatie1');
 	$(".mobileIconLijn2").toggleClass('knopAnimatie2');
 	$(".mobileIconLijn3").toggleClass('knopAnimatie3');
-		
+});
+
+
+
+
+$(window).on('resize', function(){
+	if($(this).width() <= 890){
+		$("#mobileNav").css("display", "none");
+	}
+	if($(this).width() >= 890){
+		$("#mobileNav").css("display", "inline-block");
+		$(".mobileIconLijn1").removeClass('knopAnimatie1');
+		$(".mobileIconLijn2").removeClass('knopAnimatie2');
+		$(".mobileIconLijn3").removeClass('knopAnimatie3');
+	};
 });
