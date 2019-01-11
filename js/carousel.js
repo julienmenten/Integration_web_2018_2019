ae41c5b76024d2046d7830ff4013fa4c143bd143
Seppe Clottemans <seppe.clottemans@student.ehb.be>
2019-01-11 14:22:29 +0100

    homepage fix test

--- js/carousel.js
+++ js/carousel.js
@@ -1,7 +1,7 @@
 // Gehaald van http://jsfiddle.net/X2NqX/?fbclid=IwAR0hL3W9uH8MsiVzDjfwbXY_rBcUd1CuJNFU2Ked-Ja9ohfClIke6oUyT4w
 $(function() {
   var body = $('#fotoshome');
-  var backgrounds = ['url(../images/homepagebackground.jpg)', 'url(../images/homepagebackground2.jpg)','url(../images/homepagebackground3.jpg)','url(../images/homepagebackground4.jpg)'];
+  var backgrounds = ['url(images/homepagebackground.jpg)', 'url(images/homepagebackground2.jpg)','url(images/homepagebackground3.jpg)','url(images/homepagebackground4.jpg)'];
 var current = 0;
 
 function nextBackground() {
