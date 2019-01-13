8e131daf97755eb6624cfda38656236e3c87de23
Julien Menten <julien.menten@student.ehb.Be>
2019-01-13 01:19:55 +0100

    Onnodige spaties, commentaar en functies weghalen

--- js/cookies.js
+++ js/cookies.js
@@ -1,12 +1,5 @@
 // Cookies voor het bijhouden van data tussen home page en filtering voor centra pagina 
-// visuelebegeleiding 
-// auditievebegeleiding
-// motorischebegeleiding
-
 var begeleidingKeuze = document.cookie;
-// Reminder: schrijf code om ervoor te zorgen dat er automatisch word gescrolled wanneer een bepaalde begeleiding optie is gekozen op home page, dit word wel gereset bij het surfen naar andere pagina. 
-// Bug ij autoscroll op smartphone
-
 // Verklaren van keuze op de homepage 
 if(begeleidingKeuze == ""){
 	begeleidingKeuze = "all";
@@ -30,14 +23,6 @@ function resetCookie(){
 	//begeleidingKeuze = "all";
 	//document.cookie = begeleidingKeuze;
 }
-
-/*
-// Controleren wat er in de cookie staat
-function checkCookie() {
-	//alert(document.cookie);
-}
-*/
-
 // De waarde van de select aanpassen aan de gekozen begeleidingsoptie 
 function setOption(){
 	// De gekozen specialeit vanop de homepage kiezen in de select element
@@ -51,7 +36,4 @@ function setOption(){
 	if($(window).width() <= 1193){
 		$('.filter-container').toggle().removeClass('mobileFilterToggle');
 	};
-
 }
-
-console.log(document.cookie);
--- js/mobileNav.js
+++ js/mobileNav.js
@@ -1,6 +1,5 @@
 // Declareren van variabelen
 var nav = $("#mobileNav");
-
 // Navigatie voor op kleinere schermen
 $("#mobileMenuIcon").click(function(){
 	// De navigatie tevoorschijn laten komen
@@ -10,14 +9,12 @@ $("#mobileMenuIcon").click(function(){
 	$(".mobileIconLijn2").toggleClass('knopAnimatie2');
 	$(".mobileIconLijn3").toggleClass('knopAnimatie3');
 });
-
 // Fix voor het verdwijnen van de navigatie bij resize van schermbreedte
 $(window).on('resize', function(){
 		//Bij resizen van scherm de hamburger icon resetten
 		$(".mobileIconLijn1").removeClass('knopAnimatie1');
 		$(".mobileIconLijn2").removeClass('knopAnimatie2');
 		$(".mobileIconLijn3").removeClass('knopAnimatie3');
-
 	// Bij schermen onder de 890px 
 	if($(this).width() <= 890){
 		$("#mobileNav").css("display", "none");
